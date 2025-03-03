4453a1c0263dcad3a374ead51abe765107b38ced
Leonid <leonidkorolev08@mail.ru>
2025-03-03 19:18:44 +0300

    Mars2.2

--- MARS2/main.py
+++ MARS2/main.py
@@ -11,5 +11,10 @@ def index():
     return render_template('base.html', **param)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template("training.html", prof=prof.lower())
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
