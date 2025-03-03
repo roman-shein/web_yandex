2a0612c3acb5c18ef8a27d06af0ea6a582874008
Leonid <leonidkorolev08@mail.ru>
2025-03-03 19:39:34 +0300

    Mars2.3

--- MARS2/main.py
+++ MARS2/main.py
@@ -16,5 +16,15 @@ def training(prof):
     return render_template("training.html", prof=prof.lower())
 
 
+@app.route('/list_prof/<mark>')
+def list_prof(mark):
+    li_prof = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+               'инженер по терраформированию', 'климатолог',
+               'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+               'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+               'штурман', 'пилот дронов']
+    return render_template("list_prof.html", mark=mark, list_prof=li_prof)
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
