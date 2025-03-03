0e2079d88fc322e869fbe0b0dda1139bda6e2e39
Leonid <leonidkorolev08@mail.ru>
2025-03-03 20:28:44 +0300

    Mars2.3.1

--- MARS2/main.py
+++ MARS2/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -24,6 +26,16 @@ def list_prof(mark):
                'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
                'штурман', 'пилот дронов']
     return render_template("list_prof.html", mark=mark, list_prof=li_prof)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 
 
 if __name__ == "__main__":
