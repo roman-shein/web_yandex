dde4a88373f7de56656e91283d4acf9b52c8c097
admin <roman.shein.07@mail.ru>
2025-03-31 23:35:28 +0300

    Карусель

--- Mars/Колонизация Марса.py
+++ Mars/Колонизация Марса.py
@@ -1,7 +1,6 @@
 from flask import Flask, url_for, request
 import os
 
-
 app = Flask(__name__)
 
 
@@ -302,6 +301,56 @@ def astronaut_selection():
 </html>'''
     elif request.method == 'POST':
         return "Форма отправлена"
+
+
+@app.route("/carousel")
+def carousel():
+    return f"""
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" 
+          integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" 
+          crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" 
+          integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" 
+          crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+    <title>Пример формы</title>
+</head>
+<body>
+    <h1 align="center">Пейзажи Марса</h1>
+    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <img src="{url_for('static', filename='image/mars.jpg')}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+          <img src="{url_for('static', filename='image/mars1.jpg')}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="{url_for('static', filename='image/mars2.jpg')}" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" 
+              data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" 
+              data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+</body>
+</html>
+"""
 
 
 if __name__ == "__main__":
