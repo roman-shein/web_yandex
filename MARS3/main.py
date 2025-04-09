8dfc15eee452320e7959ec098ee04df447a625f6
admin <roman.shein.07@mail.ru>
2025-04-09 17:56:52 +0300

    Форма регистрации

--- MARS3/main.py
+++ MARS3/main.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from data import db_session
 from change_image import ChangeImage
 import json
@@ -8,6 +8,7 @@ from random import choice
 from data.users import User
 from data.jobs import Jobs
 from data.departments import Department
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -153,6 +154,36 @@ def main_window():
     db_sess = db_session.create_session()
     works = db_sess.query(Jobs).all()
     return render_template("works.html", works=works)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            email=form.email.data,
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data,
+            modified_date=datetime.datetime.now()
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template("register.html", form=form, title="Регистрация")
 
 
 if __name__ == '__main__':
